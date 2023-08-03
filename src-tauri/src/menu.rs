0ff29f9405eae9f7e3100946f7b9c6e59c160717
sttk3 <>
2023-08-03 15:25:18 +0900

    macOSとそれ以外でメニューを分ける

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,7 +1,22 @@
+// use tauri::{ CustomMenuItem, Menu, Submenu } ;
 use tauri::{ Menu } ;
 
+#[cfg(target_os = "macos")]
 pub fn create(app_name: &String) -> Menu {
   let menu = tauri::Menu::os_default(app_name) ;
 
+  menu
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn create(app_name: &String) -> Menu {
+  use tauri::{ CustomMenuItem, Submenu } ;
+  
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit") ;
+  let close = CustomMenuItem::new("close".to_string(), "Close") ;
+  let file_menu = Submenu::new("File", Menu::new().add_item(quit).add_item(close)) ;
+  let menu = Menu::new()
+    .add_submenu(file_menu) ;
+  
   menu
 }
